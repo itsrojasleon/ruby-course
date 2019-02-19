884d1a42463d824e00bc0c170322cd3e0013a2b1
Juan Luis Rojas León <rojasleon122@gmail.com>
2019-02-18 19:16:50 -0600

    Integers and floats

---
+++ data-types/numbers.rb
@@ -0,0 +1,21 @@
+# INTEGERS
+a = 10
+b = 20
+
+puts a + b**10
+# Wrong
+# puts x.class z.class
+
+puts a.class
+# puts a.methods
+puts b.positive?
+
+
+# FLOATS
+puts "======="
+c = 2.1
+d = 4.0
+
+puts c / d
+
+# So cool
